892c89beb56b6145518ebd238d84519ddd1ba865
Nasrul Wahabi <wahabinasrul@gmail.com>
2023-02-24 18:15:24 +0700

    endpoints routing

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -9,6 +9,20 @@ app.UseRouting();
 app.UseEndpoints(endpoints =>
 {
     // add your end points
+    endpoints.MapGet("map1", async (context) =>
+    {
+        await context.Response.WriteAsync("in map 1");
+    });
+
+    endpoints.MapPost("map2", async (context) =>
+    {
+        await context.Response.WriteAsync("in map 2");
+    });
+});
+
+app.Run(async context =>
+{
+    await context.Response.WriteAsync($"Request receive at {context.Request.Path}");
 });
 
 app.Run();
