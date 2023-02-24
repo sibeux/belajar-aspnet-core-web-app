2aee0e3f3003b5c396ae9a824a231af3247f370f
Nasrul Wahabi <wahabinasrul@gmail.com>
2023-02-24 19:28:37 +0700

    getEndPoints

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -2,12 +2,32 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
+app.Use(async (context, next) =>
+{
+    Microsoft.AspNetCore.Http.Endpoint? endpoint = context.GetEndpoint();
+    if (endpoint != null)
+    {
+        await context.Response.WriteAsync("Endpoint: " + endpoint.DisplayName + "");
+    }
+    await next(context);
+});
+
 // enable routing
 app.UseRouting();
 
 // creating end points
 app.UseEndpoints(endpoints =>
 {
+    app.Use(async (context, next) =>
+    {
+        Microsoft.AspNetCore.Http.Endpoint? endpoint = context.GetEndpoint();
+        if (endpoint != null)
+        {
+            await context.Response.WriteAsync("Endpointt: " + endpoint.DisplayName + "\n");
+        }
+        await next(context);
+    });
+
     // add your end points
     endpoints.MapGet("map1", async (context) =>
     {
