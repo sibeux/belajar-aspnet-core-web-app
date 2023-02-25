3c929cd8068aa033c69f4064c78bac03b8305dd3
Nasrul Wahabi <wahabinasrul@gmail.com>
2023-02-25 19:08:52 +0700

    multiple action method

--- FirstApp/Controllers/HomeController.cs
+++ FirstApp/Controllers/HomeController.cs
@@ -1,11 +1,32 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace FirstApp.Controller{
-public class HomeController
+namespace FirstApp.Controller
 {
-    [Route("sayhello")]
-    public string method1(){
-        return "Hello World";
+    public class HomeController
+    {
+        [Route("home")]
+        [Route("/")]
+        public string home()
+        {
+            return "Hello from home";
+        }
+
+        [Route("index")]
+        public string index()
+        {
+            return "Hello from index";
+        }
+
+        [Route("about")]
+        public string about()
+        {
+            return "Hello from about";
+        }
+
+        [Route("user/{uid:regex(^\\d{{3}}$)}")]
+        public string UID()
+        {
+            return "Hello from UID";
+        }
     }
-}
 }
