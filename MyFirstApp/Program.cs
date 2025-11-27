3003eccef204218c41b78f494c47e315e7623c8c
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-11-27 09:15:11 +0700

    19. HTTP Response Headers

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -3,17 +3,21 @@ var app = builder.Build();
 
 app.Run(async (HttpContext context) =>
 {
-    if (1 == 1)
-    {
-    context.Response.StatusCode = 200;
+    //if (1 == 1)
+    //{
+    //context.Response.StatusCode = 200;
 
-    } else
-    {
-    context.Response.StatusCode = 500;
+    //} else
+    //{
+    //context.Response.StatusCode = 500;
 
-    }
-        await context.Response.WriteAsync("Hello");
-    await context.Response.WriteAsync(" World");
+    //}
+    context.Response.Headers["MyKey"] = "My value";
+    //await context.Response.WriteAsync("Hello");
+    //await context.Response.WriteAsync(" World");
+    context.Response.Headers["Content-Type"] = "text/html";
+    await context.Response.WriteAsync("<h1>Hello</h1>");
+    await context.Response.WriteAsync(" <h2>World</h2>");
 });
 
 app.Run();
