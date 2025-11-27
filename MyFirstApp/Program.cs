a4ec0fd7189a752a0a254e5bdad06c4f064a7c46
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-11-27 09:45:06 +0700

    20. HTTP Request

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -12,12 +12,16 @@ app.Run(async (HttpContext context) =>
     //context.Response.StatusCode = 500;
 
     //}
-    context.Response.Headers["MyKey"] = "My value";
+    //context.Response.Headers["MyKey"] = "My value";
     //await context.Response.WriteAsync("Hello");
     //await context.Response.WriteAsync(" World");
+    string path = context.Request.Path;
+    string method = context.Request.Method;
     context.Response.Headers["Content-Type"] = "text/html";
     await context.Response.WriteAsync("<h1>Hello</h1>");
     await context.Response.WriteAsync(" <h2>World</h2>");
+    await context.Response.WriteAsync($"<p>{path}</p>");
+    await context.Response.WriteAsync($"<p>{method}</p>");
 });
 
 app.Run();
