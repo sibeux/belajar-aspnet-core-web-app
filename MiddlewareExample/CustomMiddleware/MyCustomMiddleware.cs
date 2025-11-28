7033a2d8055f962615acb89c7d7e45f677e9455c
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-11-28 18:29:04 +0700

    32. Custom Middleware Extensions

---
+++ MiddlewareExample/CustomMiddleware/MyCustomMiddleware.cs
@@ -0,0 +1,19 @@
+﻿namespace MiddlewareExample.CustomMiddleware;
+
+public class MyCustomMiddleware : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        await context.Response.WriteAsync("custom middleware - start\n");
+        await next(context);
+        await context.Response.WriteAsync("custom middleware - end\n");
+    }
+}
+
+public static class CustomMiddlewareExtension
+{
+    public static IApplicationBuilder UseMyCustomMiddleware(this IApplicationBuilder app)
+    {
+        return app.UseMiddleware<MyCustomMiddleware>();
+    }
+}
--- MiddlewareExample/Program.cs
+++ MiddlewareExample/Program.cs
@@ -7,17 +7,18 @@ var app = builder.Build();
 // middleware 1
 app.Use(async (HttpContext context, RequestDelegate next) =>
 {
-    await context.Response.WriteAsync("from middleware 1");
+    await context.Response.WriteAsync("from middleware 1\n");
     await next(context);
 });
 
 // middleware 2
-app.UseMiddleware<MyCustomMiddleware>();
+//app.UseMiddleware<MyCustomMiddleware>();
+app.UseMyCustomMiddleware();
 
 // middleware 3
 app.Run(async (HttpContext context) =>
 {
-    await context.Response.WriteAsync("From middleware 3");
+    await context.Response.WriteAsync("From middleware 3\n");
 });
 
 app.Run();
