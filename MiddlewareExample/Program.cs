a16539e66347927e719250a51984e0ef2363bcac
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-11-28 13:24:38 +0700

    31. Custom Middleware Class

--- MiddlewareExample/Program.cs
+++ MiddlewareExample/Program.cs
@@ -7,24 +7,17 @@ var app = builder.Build();
 // middleware 1
 app.Use(async (HttpContext context, RequestDelegate next) =>
 {
-    await context.Response.WriteAsync("hello");
+    await context.Response.WriteAsync("from middleware 1");
     await next(context);
 });
 
 // middleware 2
-app.Use(async (context, next) =>
-{
-    await context.Response.WriteAsync("hello again");
-    await next(context);
-});
-
-// Custom middleware
 app.UseMiddleware<MyCustomMiddleware>();
 
 // middleware 3
 app.Run(async (HttpContext context) =>
 {
-    await context.Response.WriteAsync("hello again");
+    await context.Response.WriteAsync("From middleware 3");
 });
 
 app.Run();
