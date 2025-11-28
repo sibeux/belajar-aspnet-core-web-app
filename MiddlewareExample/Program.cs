1fb6901e792f2722b4e39ac49d41a3c4499aa62b
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-11-28 10:05:34 +0700

    30. Middleware Chain

--- MiddlewareExample/Program.cs
+++ MiddlewareExample/Program.cs
@@ -1,6 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+// middleware 1
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    await context.Response.WriteAsync("hello");
+    await next(context);
+});
+
+// middleware 2
+app.Use(async (context, next) =>
+{
+    await context.Response.WriteAsync("hello again");
+    await next(context);
+});
+
+// middleware 3
 app.Run(async (HttpContext context) =>
 {
     await context.Response.WriteAsync("hello again");
