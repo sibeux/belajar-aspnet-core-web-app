653d5557b57778f7ff8871a0ec5a94d8267b06e7
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-12-01 15:12:05 +0700

    40. Route Parameters

--- RoutingExample/RoutingExample/Program.cs
+++ RoutingExample/RoutingExample/Program.cs
@@ -17,9 +17,24 @@ app.MapPost("map2", async (context) =>
 
 });
 
+// eg: files/sample.txt
+app.Map("files/{filename}.{extension}", async context =>
+{
+    string? fileName = Convert.ToString(context.Request.RouteValues["filename"]);
+    string? extension = Convert.ToString(context.Request.RouteValues["extension"]);
+    await context.Response.WriteAsync($"In Files - {fileName}.{extension}");
+});
+
+// eg: employee/profile/john
+app.Map("employee/profile/{EmployeeName}", async context =>
+{
+    string? employeeName = Convert.ToString(context.Request.RouteValues["employeename"]);
+    await context.Response.WriteAsync($"In Employee - {employeeName}");
+});
+
 // fallback for any other requests
 app.MapFallback(async (context) =>
-{
+{   
     await context.Response.WriteAsync($"Request received at {context.Request.Path}");
 });
 
