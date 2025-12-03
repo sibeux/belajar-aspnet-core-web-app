e53136c8b5931e45e565787fe7544c0d5599ceaf
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-12-03 10:44:47 +0700

    70. All Model Validations - Part 1

--- ModelValidationExample/ModelValidationExample/Models/Person.cs
+++ ModelValidationExample/ModelValidationExample/Models/Person.cs
@@ -4,13 +4,16 @@ namespace ModelValidationExample.Models
 {
     public class Person
     {
-        [Required]
+        [Required(ErrorMessage = "{0} tidak boleh kosong")]
+        [Display(Name = "Nama Orang")]
         public string? PersonName { get; set;  }
-        [Required(ErrorMessage = "Email tidak boleh kosong")]
+        [Required]
         public string? Email { get; set;  }
         public string? Phone { get; set;  }
+        [StringLength(maximumLength: 12, MinimumLength = 6, ErrorMessage = "{0} harus di antara minimum:{2}-maximum:{1} digit")]
         public string? Password { get; set;  }
         public string? ConfirmPassword { get; set;  }
+        [Range(1, 99.99, ErrorMessage = "{0} should be between ${1} and ${2}")]
         public double? Price { get; set;  }
 
         public override string ToString()
