37ca2a5a905768cd22754baa28b5aee26e2ef491
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-12-04 07:36:33 +0700

    128. Services - Part 1

---
+++ DIExample/Services/CitiesService.cs
@@ -0,0 +1,22 @@
+﻿namespace Services
+{
+    public class CitiesService
+    {
+        private List<string> _cities;
+
+        public CitiesService()
+        {
+            _cities = new List<string>() {
+                "Tokyo",
+                "Bandung",
+                "Surabaya",
+                "Jakarta"
+            };
+        }
+
+        public List<string> GetCities()
+        {
+            return _cities;
+        }
+    }
+}
