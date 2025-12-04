e2b087add21f1b6f5b2ad4567befbdbacc9c4dcf
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-12-04 13:48:58 +0700

    145. Environment in Controller

--- EnvirontmentsExample/EnvirontmentsExample/Controllers/HomeController.cs
+++ EnvirontmentsExample/EnvirontmentsExample/Controllers/HomeController.cs
@@ -4,9 +4,18 @@ namespace EnvirontmentsExample.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        // constructor
+        public HomeController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
         [Route("/")]
         public IActionResult Index()
         {
+            ViewBag.CurrentEnvirontment =
+            _webHostEnvironment.EnvironmentName;
             return View();
         }
     }
