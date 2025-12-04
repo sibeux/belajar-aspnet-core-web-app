65331977f90464db2934e4fe916799002f067a88
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-12-04 11:22:18 +0700

    144. Environment in Launch Settings

---
+++ EnvirontmentsExample/EnvirontmentsExample/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EnvirontmentsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
