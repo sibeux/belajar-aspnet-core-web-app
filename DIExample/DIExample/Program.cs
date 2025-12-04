29867b72e5e25965682d9f78c9eb634356f55970
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-12-04 10:03:51 +0700

    137. AddTransient(), AddScoped(), AddSingleton()

--- DIExample/DIExample/Program.cs
+++ DIExample/DIExample/Program.cs
@@ -3,16 +3,20 @@ using Services;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
-builder.Services.Add(new ServiceDescriptor(
-    typeof(ICitiesService),
-    typeof(CitiesService),
-    // berbeda di tiap request
-    //ServiceLifetime.Transient 
-    // sama di tiap request
-    ServiceLifetime.Scoped
-// hidup selamanya, selama aplikasi tidak direstart
-//ServiceLifetime.Singleton
-));
+//builder.Services.Add(new ServiceDescriptor(
+//    typeof(ICitiesService),
+//    typeof(CitiesService),
+//    // berbeda di tiap request
+//    //ServiceLifetime.Transient 
+//    // sama di tiap request
+//    ServiceLifetime.Scoped
+//// hidup selamanya, selama aplikasi tidak direstart
+////ServiceLifetime.Singleton
+//));
+//builder.Services.AddTransient<ICitiesService, CitiesService>();
+builder.Services.AddScoped<ICitiesService, CitiesService>();
+//builder.Services.AddSingleton<ICitiesService, CitiesService>();
+
 var app = builder.Build();
 
 app.UseStaticFiles();
