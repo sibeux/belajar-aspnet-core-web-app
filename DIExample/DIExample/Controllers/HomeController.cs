7ba4b8c42a08b0a4c6885ea3d9e9519421172a27
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-12-04 09:39:08 +0700

    135. Transient, Scoped, Singleton - Part 2

--- DIExample/DIExample/Controllers/HomeController.cs
+++ DIExample/DIExample/Controllers/HomeController.cs
@@ -6,22 +6,34 @@ namespace DIExample.Controllers
 {
     public class HomeController : Controller
     {
-        /*
+        
         //private readonly CitiesService _citiesService;
-        private readonly ICitiesService _citiesService;
+        private readonly ICitiesService _citiesService1;
+        private readonly ICitiesService _citiesService2;
+        private readonly ICitiesService _citiesService3;
 
         // constructor
-        public HomeController(ICitiesService citiesService)
+        public HomeController(ICitiesService citiesService1, ICitiesService citiesService2, ICitiesService citiesService3)
         {
             // create object of CitiesService class
-            _citiesService = citiesService; //new CitiesService();
+            _citiesService1 = citiesService1; //new CitiesService();
+            _citiesService2 = citiesService2;
+            _citiesService3 = citiesService3;
         }
-        */
+        
 
         [Route("/")]
-        public IActionResult Index([FromServices] ICitiesService _citiesService)
+        //public IActionResult Index([FromServices] ICitiesService _citiesService)
+        public IActionResult Index()
         {
-            List<string> cities = _citiesService.GetCities();
+            List<string> cities = _citiesService1.GetCities();
+
+            ViewBag.InstanceId_CitiesService_1 = _citiesService1.ServiceInstanceId;
+
+            ViewBag.InstanceId_CitiesService_2 = _citiesService2.ServiceInstanceId;
+
+            ViewBag.InstanceId_CitiesService_3 = _citiesService3.ServiceInstanceId;
+             
             return View(cities);
         }
     }
--- DIExample/DIExample/Program.cs
+++ DIExample/DIExample/Program.cs
@@ -6,7 +6,12 @@ builder.Services.AddControllersWithViews();
 builder.Services.Add(new ServiceDescriptor(
     typeof(ICitiesService),
     typeof(CitiesService),
-    ServiceLifetime.Transient
+    // berbeda di tiap request
+    ServiceLifetime.Transient 
+    // sama di tiap request
+    //ServiceLifetime.Scoped
+    // hidup selamanya, selama aplikasi tidak direstart
+    //ServiceLifetime.Singleton
 ));
 var app = builder.Build();
 
