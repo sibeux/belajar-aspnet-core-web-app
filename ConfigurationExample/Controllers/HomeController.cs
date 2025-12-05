a00e0c87c7c6e93cab92d3949a1898ffd7d2dc08
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-12-05 09:37:30 +0700

    152. Hierarchical Configuration

--- ConfigurationExample/Controllers/HomeController.cs
+++ ConfigurationExample/Controllers/HomeController.cs
@@ -19,6 +19,14 @@ namespace ConfigurationExample.Controllers
             ViewBag.MyKey = _configuration["MyKey"];
             ViewBag.MyAPIKey = _configuration.GetValue("MyAPIKey", "Key not found, so it's Default value");
 
+            //ViewBag.ClientID = _configuration["weatherapi:ClientID"];
+            //ViewBag.ClientSecret = _configuration.GetValue("weatherapi:ClientSecret", "Key has been founded, so it will be ignored");
+
+            // alternative
+            IConfiguration weatherapiSection = _configuration.GetSection("weatherapi");
+            ViewBag.ClientID = weatherapiSection["ClientID"];
+            ViewBag.ClientSecret = weatherapiSection["ClientSecret"];
+
             return View();
         }
     }
