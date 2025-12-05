fc7e2b0f2069e8880e87fcf2db6db3f8555b05b7
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-12-05 14:53:14 +0700

    159. Http Client - Part 1

---
+++ StocksApp/Services/MyService.cs
@@ -0,0 +1,26 @@
+﻿namespace StocksApp.Services
+{
+    public class MyService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public MyService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public void method()
+        {
+            using (HttpClient httpClient = _httpClientFactory.CreateClient())
+            {
+                HttpRequestMessage httpRequestMessage = new HttpRequestMessage()
+                {
+                    RequestUri = new Uri("url"),
+                    Method = HttpMethod.Get,
+                };
+
+                HttpResponseMessage httpResponseMessage = 
+                httpClient.SendAsync(httpRequestMessage).Wait();
+            }
+        }
+    }
+}
