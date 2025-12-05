b8df961e5501c25be4d0003abad4d272039e835e
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-12-05 08:39:11 +0700

    150. Configuration Basics

---
+++ ConfigurationExample/Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
+app.UseEndpoints(endpoint =>
+{
+    endpoint.Map("/", async context =>
+    {
+        await context.Response.WriteAsync(app.Configuration["MyKey"] + "\n");
+        await context.Response.WriteAsync(app.Configuration.GetValue<string>("MyKey") + "\n");
+        await context.Response.WriteAsync(app.Configuration.GetValue<int>("x", 10) + "\n");
+        await context.Response.WriteAsync(app.Configuration.GetValue<int>("UnfoundKey", 10) + "\n");
+    });
+});
+
+app.MapControllers();
+
+app.Run();
