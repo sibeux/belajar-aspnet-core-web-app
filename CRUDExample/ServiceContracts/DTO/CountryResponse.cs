343d93774fd1ea8c6147e00cc9775fab97324097
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-12-08 16:22:37 +0700

    171. Get All Countries - Implementation

--- CRUDExample/ServiceContracts/DTO/CountryResponse.cs
+++ CRUDExample/ServiceContracts/DTO/CountryResponse.cs
@@ -11,10 +11,27 @@ namespace ServiceContracts.DTO
     {
         public Guid CountryID { get; set; }
         public string? CountryName { get; set; }
+
+        // it compares the current object to another object of CountryResponse type and returns true, if both values are same; otherwise returns false.
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) return false;
+            
+            if (obj.GetType() != typeof(CountryResponse)) return false;
+            CountryResponse country_to_compare = (CountryResponse)obj ;
+
+            return this.CountryID == country_to_compare.CountryID && this.CountryName == country_to_compare.CountryName;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
 
     public static class CountryExtensions
     {
+        //Converts from Country obejct to CountryResponse object
         public static CountryResponse ToCountryResponse(this Country country)
         {
             return new CountryResponse()
--- CRUDExample/Services/CountriesService.cs
+++ CRUDExample/Services/CountriesService.cs
@@ -51,7 +51,8 @@ namespace Services
 
         public List<CountryResponse> GetAllCountries()
         {
-            throw new NotImplementedException();
+            return 
+            _countries.Select(country => country.ToCountryResponse()).ToList();
         }
     }
 }
