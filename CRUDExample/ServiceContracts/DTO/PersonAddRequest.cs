59cc6ee9a40bb6597d942bb0a98c19fde60814dc
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-12-09 10:07:08 +0700

    174. Add Person - Creating Models - Part 1

---
+++ CRUDExample/ServiceContracts/DTO/PersonAddRequest.cs
@@ -0,0 +1,21 @@
+﻿using ServiceContracts.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceContracts.DTO
+{
+    /// <summary>
+    /// Acts as a DTO for inserting
+    /// </summary>
+    public class PersonAddRequest
+    {
+        public string? PersonName { get; set; }
+        public string? Email { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public GenderOptions? Gender { get; set; }
+        public Guid? CountryID { get; set; }
+        public string? Address { get; set; }
+        public bool ReceiveNewsLetters { get; set; }
+    }
+}
