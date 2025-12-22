80420cab85bc4270d02b19a6f1cbe14172bd9098
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-12-22 16:13:22 +0700

    334. register view

---
+++ ContactsManagerSolution/ContactsManager.Core/DTO/RegisterDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ContactsManager.Core.DTO
+{
+    public class RegisterDTO
+    {
+        [Required]
+        public string PersonName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [RegularExpression("^[0-9]*$")]
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
