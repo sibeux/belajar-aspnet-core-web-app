7e66e4ad85dac3caf6a68fe995035b6713d33382
Nasrul Wahabi <wahabinasrul@gmail.com>
2026-02-03 10:22:13 +0700

    367. Swagger / Open API - Basics

--- CitiesManagerSolution/CitiesManager.Web/Program.cs
+++ CitiesManagerSolution/CitiesManager.Web/Program.cs
@@ -13,6 +13,10 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Swagger
+builder.Services.AddEndpointsApiExplorer(); // generates description of all web API endpoints/action methods
+builder.Services.AddSwaggerGen(); // generates OpenAPI specification document
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,6 +27,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHsts();
 app.UseHttpsRedirection();
+
+app.UseSwagger(); // creates endpoint swagger.json
+app.UseSwaggerUI(); // creates swagger UI for testing all web API endpoints/action methods
 
 app.UseAuthorization();
 
