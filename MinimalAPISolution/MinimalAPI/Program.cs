395850569db2f5e798cbe1c7b34f8da1e7310eca
Nasrul Wahabi <wahabinasrul@gmail.com>
2026-02-04 09:57:24 +0700

    404. Creating Basic Minimal API

---
+++ MinimalAPISolution/MinimalAPI/Program.cs
@@ -0,0 +1,12 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", async (HttpContext context) => { 
+    await context.Response.WriteAsync("GET - Hello World");
+});
+
+app.MapPost("/", async (HttpContext context) => {
+    await context.Response.WriteAsync("POST - Hello World");
+});
+
+app.Run();
