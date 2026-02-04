4c2469885116f01331816b7ca0dc57c6e9fc29f4
Nasrul Wahabi <wahabinasrul@gmail.com>
2026-02-04 10:44:51 +0700

    405. GET and POST with Minimal API

---
+++ MinimalAPISolution/MinimalAPI/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace MinimalAPI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, ProductName: {ProductName}";
+        }
+    }
+}
--- MinimalAPISolution/MinimalAPI/Program.cs
+++ MinimalAPISolution/MinimalAPI/Program.cs
@@ -1,12 +1,24 @@
+using MinimalAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", async (HttpContext context) => { 
-    await context.Response.WriteAsync("GET - Hello World");
+List<Product> products = new List<Product>
+{
+    new Product { Id = 1, ProductName = "Product A" },
+    new Product { Id = 2, ProductName = "Product B" },
+    new Product { Id = 3, ProductName = "Product C" }
+};
+
+// GET /products
+app.MapGet("/products", async (HttpContext context) => {
+    var content = string.Join('\n', products.Select(temp => temp.ToString()));
+    await context.Response.WriteAsync(content);
 });
 
-app.MapPost("/", async (HttpContext context) => {
-    await context.Response.WriteAsync("POST - Hello World");
+app.MapPost("/products", async (HttpContext context, Product product) => {
+    products.Add(product);
+    await context.Response.WriteAsync("Product added successfully.");
 });
 
 app.Run();
